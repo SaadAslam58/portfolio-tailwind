b9255ce18f826195adef217c7ef39adbf5198def
SaadAslam58 <saadxpert58@gmail.com>
2024-11-08 18:28:22 +0500

    first commit

---
+++ src/app/my-projects/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import ProjectCard from '@/components/ProjectCard'
+import { Projects } from '@/constants'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div
+    style={{backgroundImage: "url(/img/mountains.jpg)"}}
+     className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
+      <div className=' gap-5 max-w-[70%] md:max-w-[80%] max-h-[80%] md:max-h-[80%] flex flex-col justify-center items-center md:grid md:grid-cols-2   '>
+        {Projects.map((project, index) => (
+          <ProjectCard
+              key={index}
+              title={project.title}
+              text={project.text}
+              image={project.src}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/constants/index.ts
@@ -0,0 +1,138 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/img/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/img/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/img/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/img/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  // {
+  //   name: "React",
+  //   Image: "/img/react.png",
+  //   width: 80,
+  //   height: 80,
+  // },
+  // {
+  //   name: "Redux",
+  //   Image: "/img/redux.png",
+  //   width: 80,
+  //   height: 80,
+  // },
+
+  {
+    name: "TypeScript",
+    Image: "/img/ts.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "Next js 13",
+    Image: "/img/next.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/img/framer.png",
+    width: 70,
+    height: 70,
+  },
+  // {
+  //   name: "Stripe Payment",
+  //   Image: "/img/stripe.webp",
+  //   width: 80,
+  //   height: 80,
+  // },
+  // {
+  //   name: "Node js",
+  //   Image: "/img/node-js.png",
+  //   width: 80,
+  //   height: 80,
+  // },
+  // {
+  //   name: "Mongo db",
+  //   Image: "/img/mongodb.png",
+  //   width: 40,
+  //   height: 40,
+  // },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/img/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/img/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/img/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "Modern Nextjs Website",
+    text: "This Website Is Made Using Nextjs, Framer Motion, and Styled Components.",
+    src: "/img/NextWebsite.png",
+    
+  },
+  {
+    title: "Space Themed Website",
+    text: "This Spacde Themed Website Is Made Using Nextjs, Framer Motion, and Styled Components.",
+    src: "/img/SpaceWebsite.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "This Modern Nextjs Portfolio Is Made Using Nextjs, Framer Motion, and Styled Components.",
+    src: "/img/WebPortfolio.png",
+  },
+  {
+    title: "Matrix themed Website",
+    text: "This Matrix Themed Website Is Made Using Nextjs, Framer Motion, and Styled Components.  ",
+    src: "/img/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
