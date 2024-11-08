912cf817c213ffd30de16ee775e0a116cf233885
SaadAslam58 <saadxpert58@gmail.com>
2024-11-09 00:43:46 +0500

    sixth commit

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div
-    style={{backgroundImage: "url(/img/mountains.jpg)"}}
+    style={{backgroundImage: "url(/img/Mountains.jpg)"}}
      className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
       <div className=' gap-5 max-w-[70%] md:max-w-[80%] max-h-[80%] md:max-h-[80%] flex flex-col justify-center items-center md:grid md:grid-cols-2   '>
         {Projects.map((project, index) => (
