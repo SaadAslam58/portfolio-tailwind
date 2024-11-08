7cb23f833e9e82c460415cbe13cfa7de639b0cbe
SaadAslam58 <saadxpert58@gmail.com>
2024-11-09 00:41:53 +0500

    sixth commit

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -8,21 +8,21 @@ const ContactForm = () => {
         <input type="text" 
         placeholder='Your Name'
         name='name'
-        className='w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow'
+        className='w-full px-3 py-2 text-sm text-black placeholder-gray-400 bg-white border-0 rounded shadow'
         />
     </div>
     <div className='mb-3'>
         <input type="email" 
         placeholder='Your Email'
         name='email'
-        className='w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow'
+        className='w-full px-3 py-2 text-sm text-black placeholder-gray-400 bg-white border-0 rounded shadow'
         />
     </div>
     <div className='mb-3'>
         <input  
         placeholder='Your Message'
         name='message'
-        className='w-full px-3 py-2 text-sm text-gray-200 placeholder-gray-400 bg-white border-0 rounded shadow'
+        className='w-full px-3 py-2 text-sm text-black placeholder-gray-400 bg-white border-0 rounded shadow'
         />
     </div>
     <button className='px-6 py-3 mt-5 font-bold text-white uppercase transition-all duration-150 ease-linear rounded-sm bg-blue-500 hover:bg-blue-400 '>
